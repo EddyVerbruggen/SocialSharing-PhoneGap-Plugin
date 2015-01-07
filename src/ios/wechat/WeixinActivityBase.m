b7f19594e992023390f5c53ade1462ec010c1e5e
Yunhai Zhu <yunhaia@gmail.com>
2015-01-07 14:07:41 -0800

    comment CFBundleDisplayName

--- src/ios/wechat/WeixinActivityBase.m
+++ src/ios/wechat/WeixinActivityBase.m
@@ -111,7 +111,7 @@
     //    req.bText = NO;
     req.message = WXMediaMessage.message;
     if (scene == WXSceneSession) {
-        req.message.title = [NSString stringWithFormat:NSLocalizedString(@"%@ Share",nil), NSLocalizedStringFromTable(@"CFBundleDisplayName", @"InfoPlist", nil)];
+        //req.message.title = [NSString stringWithFormat:NSLocalizedString(@"%@ Share",nil), NSLocalizedStringFromTable(@"CFBundleDisplayName", @"InfoPlist", nil)];
         req.message.description = title;
     } else {
         req.message.title = title;
