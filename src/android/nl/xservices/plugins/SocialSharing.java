fa243d4230e3b82c59aeef29cfba9b67c50ab50e
SebT <SebT@users.noreply.github.com>
2015-10-28 16:32:47 +0100

    Fixed missing comma

--- src/android/nl/xservices/plugins/SocialSharing.java
+++ src/android/nl/xservices/plugins/SocialSharing.java
@@ -18,7 +18,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
-import org.json.JSONArray
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
