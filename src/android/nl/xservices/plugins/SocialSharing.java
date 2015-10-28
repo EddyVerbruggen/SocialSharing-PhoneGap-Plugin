85022b95ade402fd585932232f26ce2c71bb810a
SebT <SebT@users.noreply.github.com>
2015-10-28 14:47:19 +0100

    Used API number value instead of constant

--- src/android/nl/xservices/plugins/SocialSharing.java
+++ src/android/nl/xservices/plugins/SocialSharing.java
@@ -18,7 +18,7 @@ import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
-import org.json.JSONArray;
+import org.json.JSONArray
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -233,7 +233,7 @@ public class SocialSharing extends CordovaPlugin {
         if (notEmpty(message)) {
           sendIntent.putExtra(android.content.Intent.EXTRA_TEXT, message);
           // sometimes required when the user picks share via sms
-          if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+          if (Build.VERSION.SDK_INT < 22) { // LOLLIPOP
             sendIntent.putExtra("sms_body", message);
           }
         }
