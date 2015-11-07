0f1603b555482d86d2798f9a6dcbf63e6b8c2ad0
jmail <j.krzemienski@webzzmaster.pl>
2015-11-07 04:08:28 +0100

    shareViaSMS functionality for windows phone 8
    using standard functionality + getting message from json using Newtonsoft.Json.dll

--- src/wp8/SocialSharing.cs
+++ src/wp8/SocialSharing.cs
@@ -4,6 +4,8 @@ using WPCordovaClassLib.Cordova;
 using WPCordovaClassLib.Cordova.Commands;
 using WPCordovaClassLib.Cordova.JSON;
 
+using Newtonsoft.Json;
+
 namespace Cordova.Extension.Commands
 {
     public class SocialSharing : BaseCommand
@@ -76,5 +78,26 @@ namespace Cordova.Extension.Commands
             draft.Show();
             DispatchCommandResult(new PluginResult(PluginResult.Status.OK, true));
         }
+		
+		 public void shareViaSMS(string jsonArgs)
+        {
+            var options = JsonHelper.Deserialize<string[]>(jsonArgs);
+
+            SmsComposeTask smsComposeTask = new SmsComposeTask();
+
+			smsComposeTask.To = options[1];
+            SMSMessageClass m = JsonConvert.DeserializeObject<SMSMessageClass>(options[0]);
+            smsComposeTask.Body = m.message;
+
+			smsComposeTask.Show();
+
+			DispatchCommandResult(new PluginResult(PluginResult.Status.OK, true));
+        }
     }
+
+    public class SMSMessageClass
+    {
+        public string message { get; set; }
+    }
+
 }
