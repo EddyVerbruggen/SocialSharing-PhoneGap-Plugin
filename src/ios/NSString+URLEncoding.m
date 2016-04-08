305faad10a8c735162190e3d7ec95ed797e62656
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-04-08 15:53:11 +0200

    #564 Sharing on IOS encodes special characters (more than one #)

--- src/ios/NSString+URLEncoding.m
+++ src/ios/NSString+URLEncoding.m
@@ -3,12 +3,28 @@
 @implementation NSString (URLEncoding)
 - (NSString*)URLEncodedString
 {
-  return (NSString *)CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(
-                            kCFAllocatorDefault,
-                            (CFStringRef)self,
-                            CFSTR("#"), // don't escape these
-                            NULL, // allow escaping these
-                            kCFStringEncodingUTF8
-                      ));
+  NSString* result = (NSString *)CFBridgingRelease(
+                          CFURLCreateStringByAddingPercentEscapes(
+                                kCFAllocatorDefault,
+                                (CFStringRef)self,
+                                CFSTR("#"), // don't escape these
+                                NULL, // allow escaping these
+                                kCFStringEncodingUTF8
+                          )
+                     );
+  
+  // we may have a URL with more than one '#' now - which iOS doesn't allow, so escape all but the first one
+  NSArray *parts = [result componentsSeparatedByString:@"#"];
+  NSString *finalResult = parts[0];
+  for (int i=1; i<parts.count; i++) {
+    NSString *part = [parts objectAtIndex:i];
+    if (i==1) {
+      finalResult = [finalResult stringByAppendingString:@"#"];
+    } else {
+      finalResult = [finalResult stringByAppendingString:@"%23"];
+    }
+    finalResult = [finalResult stringByAppendingString:part];
+  }
+  return finalResult;
 }
 @end
