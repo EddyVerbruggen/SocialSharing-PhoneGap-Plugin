b092b8e9066b0ceb7b6d94ab22f7694ae6fd07e7
EddyVerbruggen <eddyverbruggen@gmail.com>
2019-06-06 09:31:51 +0200

    bump

--- www/SocialSharing.js
+++ www/SocialSharing.js
@@ -1,9 +1,9 @@
 function SocialSharing() {
-} 
+}
 
 // Override this method (after deviceready) to set the location where you want the iPad popup arrow to appear.
 // If not overridden with different values, the popup is not used. Example:
-// 
+//
 //   window.plugins.socialsharing.iPadPopupCoordinates = function() {
 //     return "100,100,200,300";
 //   };
@@ -137,12 +137,13 @@ SocialSharing.install = function () {
   }
 
   window.plugins.socialsharing = new SocialSharing();
-  
+
   // Note only polyfill navigator.share if it is not defined, since shareW3C implements L1 of the spec,
   // and an existing navigator.share method could implement L2.
-  if (!navigator.share)
+  if (!navigator.share) {
     navigator.share = window.plugins.socialsharing.shareW3C;
-  
+  }
+
   return window.plugins.socialsharing;
 };
 
