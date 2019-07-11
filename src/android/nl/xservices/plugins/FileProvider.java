d6a3a93fd2599a690e45c62b7112ac7b0c143295
Cairin Michie <cairinmichie@gmail.com>
2019-07-11 15:48:17 +0200

    Update FileProvider path for androidx

--- src/android/nl/xservices/plugins/FileProvider.java
+++ src/android/nl/xservices/plugins/FileProvider.java
@@ -1,5 +1,5 @@
 package nl.xservices.plugins;
 
 
-public class FileProvider extends android.support.v4.content.FileProvider {
+public class FileProvider extends androidx.core.content.FileProvider {
 }
